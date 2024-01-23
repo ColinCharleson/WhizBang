a91c1355b734342dc2571b15e39057cc038f25e5
mark-Toufic <72156370+mark-Toufic@users.noreply.github.com>
2024-01-23 01:21:59 -0500

    gun code

--- Whiz Bang/Assets/Scripts/GunSystem.cs
+++ Whiz Bang/Assets/Scripts/GunSystem.cs
@@ -20,16 +20,79 @@ public class GunSystem : MonoBehaviour
     public RaycastHit rayHit;
     public LayerMask whatIsEnemy;
 
+    private void awake()
+    {
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
+
+    private void Update()
+    {
+        MyInput();
+    }
 
     private void MyInput()
     {
- /*       if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse(0);
+        if (fullAuto) shooting = Input.GetKey(KeyCode.Mouse0);
 
         else shooting = Input.GetKeyDown(KeyCode.Mouse0);
 
-        if(Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading)
+        {
+            Reload();
+        }
 
-        */
+        //shot
+        if(readyToShoot && shooting && !reloading && bulletsLeft > 0)
+        {
+            bulletsShot = burst;
+            Shoot();
+        }
     }
+
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", realodTime);
+    }
+
+    private void ReloadFinished()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        Vector3 direction = cam.transform.forward + new Vector3(x, y, 0);
+
+        //RayCast
+        if (Physics.Raycast(cam.transform.position, direction, out rayHit, range, whatIsEnemy))
+        {
+           /* Debug.Log(rayHit.collider.name);
+            if(rayHit.collider.CompareTag("Enemy"))
+            {
+                rayHit.collider.GetComponent<Enemy>().TakeDamage(damage);
+            }*/
+        }
+
+        bulletsLeft--;
+        bulletsShot--;
+
+        Invoke("ResetSHot", timeBetweenShooting);
+
+        if(bulletsShot > 0 && bulletsLeft > 0)
+        Invoke("Shoot", timeBetweenShots);
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+    }
+
 
 }
